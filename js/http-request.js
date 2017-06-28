d7646410b8513b0b2f7789fb122fb6e3b6f346da
George Campbell <campbelg@oclc.org>
2017-06-28 16:19:44 -0400

    Updates

--- js/http-request.js
+++ js/http-request.js
@@ -3,20 +3,80 @@
 (function () {
     "use strict";
 
-    /** Create a server **/
-    let http = require("http");
+    /**
+     * Create a server listening on port 8090 using http
+     */
+    function server() {
+        let http = require("http");
 
-    http.createServer(function (request, response) {
+        http.createServer(function (request, response) {
 
-        response.statusCode = 200;
-        response.setHeader("Content-Type", "text/html");
+            response.statusCode = 200;
+            response.setHeader("Content-Type", "text/html");
 
-        response.write("Now is the time for all good men to come to the aid of their country.");
-        response.end();
+            response.write("Now is the time for all good men to come to the aid of their country.");
+            response.end();
 
-    }).listen(8090);
+        }).listen(8090);
+    }
 
-    /** Make an http request **/
-    //TODO
+    /**
+     * Make a GET request to an https site
+     */
+    function get() {
+        let https = require("https"),
+            options = {
+                host: 'www.metaweather.com',
+                path: '/api/location/search/?query=Columbus'
+            },
+            callback = function (response) {
+                let str = '';
+
+                //another chunk of data has been received, so append it to `str`
+                response.on('data', function (chunk) {
+                    str += chunk;
+                });
+
+                //the whole response has been recieved, so we just print it out here
+                response.on('end', function () {
+                    console.log(str);
+                });
+            };
+
+        https.request(options, callback).end();
+    }
+
+    /**
+     * Make a POST request to an http site
+     */
+    function post() {
+        let http = require("http"),
+            options = {
+                host: 'www.nodejitsu.com',
+                path: '/',
+                port: '1337',
+                method: 'POST'
+            },
+            callback = function (response) {
+                var str = ''
+                response.on('data', function (chunk) {
+                    str += chunk;
+                });
+
+                response.on('end', function () {
+                    console.log(str);
+                });
+            },
+            req = http.request(options, callback);
+
+        req.write("hello world!");
+        req.end();
+    }
+
+    /*********************************************************************************/
+
+    //server();
+    //get();
+    post();
 
 }());
