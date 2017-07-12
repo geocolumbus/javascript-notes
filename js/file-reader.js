66aa29f3ea0747b0df036616dc5b93a5588ceb35
George Campbell <campbelg@oclc.org>
2017-07-12 10:58:05 -0400

    Add file readers.

--- js/file-reader.js
+++ js/file-reader.js
@@ -3,24 +3,27 @@
 // READ A TEXT FILE ********************************************************
 
 let textFile = require("fs");
+let text;
 
 textFile.readFile("../res/data.txt", "utf8", function (err, data) {
     if (err) {
         return console.log(err);
     }
-    console.log("\n" + data);
+    text = data;
+    console.log("\n" + text);
 });
 
 // READ A JSON FILE ********************************************************
 
 let jsonFile = require("fs");
+let json;
 
 jsonFile.readFile("../res/data.json", "utf8", function (err, data) {
     if (err) {
         return console.log(err);
     }
     try {
-        let json = JSON.parse(data);
+        json = JSON.parse(data);
         console.log("\n" + JSON.stringify(json, null, 4));
     } catch (error) {
         console.log(error);
@@ -32,18 +35,17 @@ jsonFile.readFile("../res/data.json", "utf8", function (err, data) {
 // npm install yamljs
 // add to .bash_profile export PATH=$PATH:node_modules/.bin
 
-
 let yamlFile = require("fs");
 let YAML = require("yamljs");
-
+let yaml;
 
 yamlFile.readFile("../res/data.yaml", "utf8", function (err, data) {
     if (err) {
         return console.log(err);
     }
     try {
-        let nativeObject = YAML.parse(data);
-        console.log(JSON.stringify(nativeObject, null, 4));
+        yaml = YAML.parse(data);
+        console.log(JSON.stringify(yaml, null, 4));
     } catch (error) {
         console.log(error);
     }
