42bacb1a60ed647dc30af8e5d001cbc3cfa1bb18
George Campbell <campbelg@oclc.org>
2017-10-17 10:01:37 -0400

    async

--- js/promises.js
+++ js/promises.js
@@ -4,81 +4,79 @@
 
 (function () {
     "use strict";
-    /*
-        let p1 = function () {
-            return new Promise(function (resolve, reject) {
-                setTimeout(function () {
-                    resolve("p1 OK ");
-                }, 800);
-            });
-        };
+    let p1 = function () {
+        return new Promise(function (resolve, reject) {
+            setTimeout(function () {
+                resolve("p1 OK ");
+            }, 800);
+        });
+    };
 
-        let p2 = function () {
-            return new Promise(function (resolve, reject) {
-                setTimeout(function () {
-                    resolve("p2 OK ");
-                }, 200);
-            });
-        };
+    let p2 = function () {
+        return new Promise(function (resolve, reject) {
+            setTimeout(function () {
+                resolve("p2 OK ");
+            }, 200);
+        });
+    };
 
-        let p3 = function () {
-            return new Promise(function (resolve, reject) {
-                setTimeout(function () {
-                    resolve("p3 OK ");
-                }, 1200);
-            });
-        };
+    let p3 = function () {
+        return new Promise(function (resolve, reject) {
+            setTimeout(function () {
+                resolve("p3 OK ");
+            }, 1200);
+        });
+    };
 
-        let resCon = function (res) {
-            console.log(`Consecutively resolved: ${res}`);
-        };
-        let resSim = function (res) {
-            console.log(`Simultaneously resolved: ${res}`);
-        };
-        let resRace = function (res) {
-            console.log(`Race resolved: ${res}`);
-        };
-        let rej = function (rej) {
-            console.log(`resolved: ${rej}`);
-        };
+    let resCon = function (res) {
+        console.log(`Consecutively resolved: ${res}`);
+    };
+    let resSim = function (res) {
+        console.log(`Simultaneously resolved: ${res}`);
+    };
+    let resRace = function (res) {
+        console.log(`Race resolved: ${res}`);
+    };
+    let rej = function (rej) {
+        console.log(`resolved: ${rej}`);
+    };
 
-        // Resolve consecutively - depend on each step
-        p1()
-            .then(p1)
-            .then(p2)
-            .then(p3)
-            .then(resCon)
-            .catch(rej);
+    // Resolve consecutively - depend on each step
+    p1()
+        .then(p1)
+        .then(p2)
+        .then(p3)
+        .then(resCon)
+        .catch(rej);
 
-        // Resolve simultaneously - independant of each other
-        Promise.all([p1(), p2(), p3()]).then(resSim).catch(rej);
+    // Resolve simultaneously - independant of each other
+    Promise.all([p1(), p2(), p3()]).then(resSim).catch(rej);
 
-        // Resolve the first that resolves
-        Promise.race([p1(), p2(), p3()]).then(resRace).catch(rej);
+    // Resolve the first that resolves
+    Promise.race([p1(), p2(), p3()]).then(resRace).catch(rej);
 
-        // Promise looping
+    // Promise looping
 
     // Given async function sayHi
-        function sayHi() {
-            return new Promise((resolve) => {
-                setTimeout(() => {
-                    console.log('Hi');
-                    resolve();
-                }, 50);
-            });
-        }
+    function sayHi() {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                console.log('Hi');
+                resolve();
+            }, 50);
+        });
+    }
 
     // And an array of async functions to loop through
-        const asyncArray = [sayHi, sayHi, sayHi];
+    const asyncArray = [sayHi, sayHi, sayHi];
 
     // We create the start of a promise chain
-        let chain = Promise.resolve();
+    let chain = Promise.resolve();
 
     // And append each function in the array to the promise chain
-        for (const func of asyncArray) {
-            chain = chain.then(func);
-        }
-    */
+    for (const func of asyncArray) {
+        chain = chain.then(func);
+    }
 
     // async from ES2017
 
