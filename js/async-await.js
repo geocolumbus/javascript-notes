bb848e03268cbd9fd0c24fba6ba95c1d760d5971
Campbell <campbelg@campbelg-3mbpdu.oa.oclc.org>
2018-11-16 11:03:22 -0500

    Enhanced async/await with looping

--- js/async-await.js
+++ js/async-await.js
@@ -27,4 +27,29 @@
         console.log(c1);
     })();
 
+    // LOOPING
+    const square = (num = 0) => {
+        console.log(`squaring ${num}`);
+        return new Promise((res, rej) => {
+            setTimeout(() => {
+                res(num * num);
+            }, 250);
+        });
+    };
+
+    let nums = [1, 2, 3, 4, 5, 6];
+
+    async function processArray(nums) {
+        let sum = 0;
+        for (const num of nums) {
+            sum += await square(num);
+        }
+        return sum;
+    }
+
+    processArray(nums).then(sum => {
+        console.log(`Sum of squares = ${sum}`);
+    });
+
 }());
+
