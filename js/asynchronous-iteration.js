061c55ffc783c2633a1126073dde0de6e0d1d5a8
Campbell <campbelg@campbelg-3mbpdu.oa.oclc.org>
2019-06-04 14:37:04 -0400

    Syntax not supported in IntelliJ nor node for asynch-iter

--- js/asynchronous-iteration.js
+++ js/asynchronous-iteration.js
@@ -1,6 +1,9 @@
 /*jshint esversion: 6 */
 // Requires ES2018
 
+// TODO - Syntax not supported in IntelliJ & functionality not in node ... consider Babel or wait for future Node releases
+/*
 for await (const line of readlines(filePath)) {
     console.log(line);
 }
+*/
