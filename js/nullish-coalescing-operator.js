b1b46006d26fc4eb5eb341980a1af519b9333a53
geocolumbus <geocolumbus@gmail.com>
2020-12-02 10:07:14 -0500

    Add nullish coalescing operator

---
+++ js/nullish-coalescing-operator.js
@@ -0,0 +1,16 @@
+"use strict"
+// Supported in node 14
+// jshint has no idea
+
+const a = false ?? "string"
+const b = NaN ?? "string"
+const c = undefined ?? "string"
+
+// false NaN string
+console.log(a, b, c)
+
+let d
+const e = d?.person?.name ?? "Handled deep fail"
+
+// Handled deep fail
+console.log(e)
